185ef2cfb85838e3dcdf19cba17bd8d19f774c8b
slemesle <seven.lemesle@wescale.fr>
2017-07-10 15:03:50 +0200

    Define complete struct for player state

--- cmn/game.go
+++ cmn/game.go
@@ -21,7 +21,26 @@ func (state State) string() string {
 	return states[state-1]
 }
 
-
-type Game struct {
+type Board struct {
 
 }
+
+type GameState struct {
+	Partie State `json:"partie"`
+	Turn string `json:"turn"`
+	Grid Board `json:"grid_lines"`
+}
+
+
+/*
+
+{
+  “partie”: WAIT | RUN | LOST | WIN,
+  “turn”: None | {joueur-uuid}
+  “grid_lines”: [
+     { “0”: - | (joueur-uuid), “1”: - | (joueur-uuid), “2”: - | (joueur-uuid) },
+     { “0”: - | (joueur-uuid), “1”: - | (joueur-uuid), “2”: - | (joueur-uuid) },
+     { “0”: - | (joueur-uuid), “1”: - | (joueur-uuid), “2”: - | (joueur-uuid) },
+   ]
+}
+ */
